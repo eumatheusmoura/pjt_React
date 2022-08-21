644cf3516457f99c34bc07d4f53916c5445aff09
Matheus Moura <matheusmouraa@icloud.com>
2022-08-21 16:10:09 -0300

    feat: criando e adicionando o componente footer

---
+++ mock_modulo5/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import S from "../Footer/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={S.Footer}>
+      <p>Testando suas habilidades em HTML, CSS e JS</p>
+      <p>Linx Impulse</p>
+      <p>2019</p>
+    </div>
+  );
+};
+
+export default Footer;
